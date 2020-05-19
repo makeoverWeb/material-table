d825e0ece2387374c5b76aa76dfc1227d00d1956
makeoverweb <femakeoverweb@gmail.com>
2020-05-19 19:46:39 +0300

    six commit

--- src/http/get-api/index.js
+++ src/http/get-api/index.js
@@ -1,12 +1,13 @@
-exports.handler = async function http (req) {
-  console.log('Begin API called')
+const { REACT_APP_API_URL } = process.env;
+
+exports.handler = async function http(req) {
+  const response = await fetch(`${REACT_APP_API_URL}`);
+    const result = await response.json();
   return {
     headers: {
       'content-type': 'application/json; charset=utf8',
       'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
     },
-    body: JSON.stringify({
-      message: 'Hello from your Begin API!'
-    })
+    body: JSON.stringify(req)
   }
 }
